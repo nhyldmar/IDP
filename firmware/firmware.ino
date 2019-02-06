55bb923c5c026f55f7dcce6a9f9b136c7724c040
Nicholas Hyldmar <nh490@cam.ac.uk>
2019-02-06 11:42:47 +0000

    Fixed communcation

---
+++ firmware/firmware.ino
@@ -0,0 +1,140 @@
+/*  This is the main file for the Orange Pip.
+ *  When the infrared sensor returns a signal, it initiates the detection sequence.
+ *  
+ *  On the first round it follows the walls with the ultrasonic sensor and a proportional controller,
+ *  intersecting with the five known cells, then returns to the safe zone and back to the opposite
+ *  starting zone.
+ *  
+ *  On the second round it receives motor speeds as two signed 8-bit integers through serial and sets them.
+ *  It will move towards the nearest block until the detection sequence initiates.
+ *  Once all the blocks are passed, it returns to the safe zone and back to a starting zone.
+ *  
+ *  The detection sequence can be understood by observation.
+ *  
+ *  Speed is an integer from -255 to 255 (positive = forwards, negative = backwards).
+ *  The servo will move between 30 and 150 degrees, where 150 is the closed state and 30 is the open state.
+ *  
+ *  TODO: Calibrate time delays.
+ *  TODO: Merge first round (backup.ino) into code.
+ */
+
+
+#include <Adafruit_MotorShield.h>
+#include <Servo.h>
+
+// Initiate motors
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *L_motor = AFMS.getMotor(1);  // Left motor on port 1
+Adafruit_DCMotor *R_motor = AFMS.getMotor(2);  // Right motor on port 2
+int8_t op_speed = 100;  // Operational speed for tasks
+
+// Initiate servo
+int servo_pin = 9;
+Servo servo;
+int open_angle = 30;
+int closed_angle = 150;
+
+// Define infrared sensor and Hall effect sensor inputs
+int IR_pin = 12;
+int HES_pin = 13;
+
+int iteration;
+
+void setMotorSpeed(Adafruit_DCMotor *motor, int motor_speed) {
+  motor->setSpeed(abs(motor_speed));
+  if (motor_speed >= 0) {
+    motor->run(FORWARD);
+  }
+  else if (motor_speed < 0) {
+    motor->run(BACKWARD);
+  }
+}
+
+void setMotorSpeeds(int L_speed, int R_speed) {
+  setMotorSpeed(L_motor, L_speed);
+  setMotorSpeed(R_motor, R_speed);
+}
+
+void approach() {
+  setMotorSpeeds(op_speed / 2, op_speed / 2);
+  delay(100);
+  setMotorSpeeds(0, 0);
+}
+
+void collect() {
+  setMotorSpeeds(-op_speed, -op_speed);
+  delay(100);
+  setMotorSpeeds(0, 0);
+  servo.write(open_angle);
+  setMotorSpeeds(op_speed, op_speed);
+  delay(100);
+  setMotorSpeeds(0, 0);
+  servo.write(closed_angle);
+}
+
+void push() {
+  servo.write(open_angle);
+  delay(100);
+  servo.write(closed_angle);
+}
+
+void drop() {
+  servo.write(open_angle);
+  delay(100);
+  setMotorSpeeds(op_speed, op_speed);
+  delay(100);
+  setMotorSpeeds(0, 0);
+  delay(100);
+  setMotorSpeeds(-op_speed, -op_speed);
+  delay(100);
+  servo.write(closed_angle);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print('r');
+  AFMS.begin();
+  setMotorSpeeds(0, 0);
+  servo.attach(servo_pin);
+  servo.write(closed_angle);
+}
+
+void loop() {
+  // Get data
+  if (Serial.available() > 0) {
+    iteration = 0;
+    char order = Serial.read();
+    if (order == 's') {
+      int8_t L_speed = Serial.read();
+      int8_t R_speed = Serial.read();
+      Serial.print(L_speed);
+      Serial.println(R_speed);
+      Serial.print('r');
+      setMotorSpeeds(L_speed, R_speed);
+    }
+    else if (order == 'd') {
+      drop();
+      Serial.print('r');
+    }
+  }
+
+  // Resend confirmation if there is no communciation
+  iteration++;
+  if (iteration >= 1023) {
+    Serial.print('r');
+    iteration = 0;
+  }
+  
+  // Detection sequence
+  if (analogRead(IR_pin) > 1024) {  // TODO: Calibrate infrared input threshold
+    approach();
+    if (analogRead(HES_pin) > 1024) {  // TODO: Calibrate Hall effect sensor input threshold
+      push();
+    }
+    else {
+      collect();
+    }
+  }
+  
+  delay(50);
+}
--- main.py
+++ main.py
@@ -17,10 +17,9 @@ import struct
 #                       (130, 140, 20), (200, 220, 130)     (green)
 
 # User inputs
-streaming = True
-controlled = False
+streaming = False
+controlled = True
 camera = 2
-port = 'COM3'
 image_name = "test_data/stream.jpg"
 
 # Parameters
@@ -35,6 +34,8 @@ robot_boundaries = np.array(robot_boundaries, dtype="uint8")
 cell_thresholds = ((4, 4), (10, 10))
 robot_thresholds = ((20, 20), (40, 40))
 image_boundaries = ((0, 640), (0, 550))
+
+distance_offset = 35
 
 
 def startStream(camera):
@@ -52,19 +53,33 @@ def startStream(camera):
     return stream
 
 
-def startSerial(port):
+def startSerial():
     """Given a port, tries to start a serial stream and returns it"""
-    try:
-        board = serial.Serial(port, 115200, timeout=.1)
-        print("Sending...")
-    except serial.serialutil.SerialException:
-        print("No board detected at {}".format(port))
+    ports = ["COM{}".format(port) for port in range(255)]
+    active_ports = []
+    board = None
+    for port in ports:
+        try:
+            board = serial.Serial(port, timeout=1)
+            active_ports.append(port)
+        except serial.serialutil.SerialException:
+            pass
+
+    if len(active_ports) == 0:
+        print("No COM ports open")
         sys.exit()
+    elif len(active_ports) > 1:
+        board.close()
+        active_ports = input("COM ports {} are open. Please select one:".format(active_ports))
+        active_ports = ["COM{}".format(active_ports)]
+        board = serial.Serial(*active_ports, timeout=1)
+
+    print("Sending to {}...".format(*active_ports))
 
     return board
 
 
-def readColour(image, boundaries, thresholds):
+def readColour(boundaries, thresholds):
     """Given an image, return image with bounding boxes and a colour mask for a given colour boundary set."""
     coordinates = np.array([])
     images_mask = np.zeros(image.shape, dtype="uint8")
@@ -97,8 +112,8 @@ def readImage(image):
     image = image[image_boundaries[0][0]:image_boundaries[0][1], image_boundaries[1][0]:image_boundaries[1][1]]
 
     # Get data from cells and robot colour masks
-    cell_mask, cell_coordinates = readColour(image, cell_boundaries, cell_thresholds)
-    robot_mask, robot_coordinates = readColour(image, robot_boundaries, robot_thresholds)
+    cell_mask, cell_coordinates = readColour(cell_boundaries, cell_thresholds)
+    robot_mask, robot_coordinates = readColour(robot_boundaries, robot_thresholds)
 
     # Combine colour masks
     mask = cv2.bitwise_or(cell_mask, robot_mask)
@@ -108,6 +123,7 @@ def readImage(image):
         robot_centre = np.average(robot_coordinates.T, axis=1)
         robot_direction = np.subtract(*robot_coordinates)
         robot_direction = robot_direction / np.linalg.norm(robot_direction)
+        robot_centre += robot_direction * distance_offset
     else:
         robot_centre = [0, 0]
         robot_direction = [0, 0]
@@ -118,7 +134,7 @@ def readImage(image):
     return image, cell_coordinates, (robot_centre, robot_direction), mask
 
 
-def findNearestCell(image, robot_centre, robot_direction, cell_coordinates):
+def findNearestCell(robot_centre, robot_direction, cell_coordinates):
     """Given the current positions of everything, return the coordinates of and distance and angle to the closest
     cell"""
     if cell_coordinates.shape[0] and robot_centre[0]:
@@ -156,16 +172,20 @@ def approachCell(min_distance, min_angle):
     return motor_speeds
 
 
-def sendSpeeds(board, motor_speeds):
+def sendSpeeds(motor_speeds):
     """Given motor speeds, sends them to the board."""
-    motor_speeds = [speed * 100 for speed in motor_speeds]
-    motor_speeds = np.array(motor_speeds, dtype="int8")
-    buffer = bytearray(struct.pack('cbb', b's', *motor_speeds))
     if board.read() == b'r':
+        motor_speeds = [speed * 127 for speed in motor_speeds]
+        motor_speeds = np.array(motor_speeds, dtype="int8")
+        buffer = bytearray(struct.pack('cbb', b's', *motor_speeds))
         board.write(buffer)
 
+        sent = struct.unpack('cbb', buffer)[1:]
+        received = board.readline().decode()
+        print("Sent: {}{}  Received: {}".format(*sent, received))
 
-def processPlayer(image):
+
+def processPlayer():
     """Checks for keyboard inputs."""
     # Check for directional input
     if keyboard.is_pressed('up'):
@@ -184,9 +204,10 @@ def processPlayer(image):
         motor_speeds = (0, 0)
         arrow_direction = (0, 0)
 
-    arrow_start = np.subtract(image.shape[:-1], (50, 100))
-    arrow_direction = 10 * np.array(arrow_direction)
-    drawArrow(image, arrow_start, arrow_direction)
+    if image:
+        arrow_start = np.subtract(image.shape[:-1], (50, 100))
+        arrow_direction = 10 * np.array(arrow_direction)
+        drawArrow(image, arrow_start, arrow_direction)
 
     return motor_speeds
 
@@ -195,7 +216,7 @@ def drawArrow(image, start, direction):
     """Given an image, a point and a direction, draws a line on the image."""
     start = np.array(start, dtype="int16")
     direction = np.array(direction)
-    end = start + 50 * direction
+    end = start + 30 * direction
     end = np.array(end, dtype="int16")
     start = tuple(start)
     end = tuple(end)
@@ -204,7 +225,7 @@ def drawArrow(image, start, direction):
 
 if streaming and controlled:
     stream = startStream(camera)
-    board = startSerial(port)
+    board = startSerial()
 
     # Main loop for streaming
     is_on = True
@@ -219,21 +240,22 @@ if streaming and controlled:
 
         image, cell_coordinates, (robot_centre, robot_direction), mask = readImage(image)
 
-        min_coordinates, min_distance, min_angle = findNearestCell(image, robot_centre, robot_direction,
+        min_coordinates, min_distance, min_angle = findNearestCell(robot_centre, robot_direction,
                                                                    cell_coordinates)
 
-        motor_speeds = processPlayer(image)
+        motor_speeds = processPlayer()
 
-        sendSpeeds(board, motor_speeds)
+        sendSpeeds(motor_speeds)
 
         cv2.imshow("Stream", np.hstack((image, mask)))
 
     stream.release()
+    board.close()
 
 elif streaming:
     stream = startStream(camera)
 
-    board = startSerial(port)
+    board = startSerial()
 
     # Main loop for streaming
     is_on = True
@@ -248,23 +270,34 @@ elif streaming:
 
         image, cell_coordinates, (robot_centre, robot_direction), mask = readImage(image)
 
-        min_coordinates, min_distance, min_angle = findNearestCell(image, robot_centre, robot_direction,
+        min_coordinates, min_distance, min_angle = findNearestCell(robot_centre, robot_direction,
                                                                    cell_coordinates)
 
         motor_speeds = approachCell(min_distance, min_angle)
 
-        sendSpeeds(board, motor_speeds)
+        sendSpeeds(motor_speeds)
 
         cv2.imshow("Stream", np.hstack((image, mask)))
 
     stream.release()
+    board.close()
+
+elif controlled:
+    board = startSerial()
+    image = False
+    while True:
+        motor_speeds = processPlayer()
+        sendSpeeds(motor_speeds)
+        if keyboard.is_pressed('esc'):
+            break
+    board.close()
 
 else:
     image = cv2.imread(image_name)
 
     image, cell_coordinates, (robot_centre, robot_direction), mask = readImage(image)
 
-    min_coordinates, min_distance, min_angle = findNearestCell(image, robot_centre, robot_direction,
+    min_coordinates, min_distance, min_angle = findNearestCell(robot_centre, robot_direction,
                                                                cell_coordinates)
 
     motor_speeds = approachCell(min_distance, min_angle)
@@ -272,3 +305,5 @@ else:
     cv2.imwrite("vision.jpg", np.hstack((image, mask)))
 
 cv2.destroyAllWindows()
+
+print("Done")
